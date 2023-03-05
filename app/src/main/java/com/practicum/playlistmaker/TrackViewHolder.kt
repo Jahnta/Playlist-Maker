533fc1df73d0d9af5ef67c41623f799ed4a66039
Jahnta <dabrusov@gmail.com>
2023-03-05 23:39:48 +0300

    Задача спринта №10

---
+++ app/src/main/java/com/practicum/playlistmaker/TrackViewHolder.kt
@@ -0,0 +1,35 @@
+package com.practicum.playlistmaker
+
+import android.view.RoundedCorner
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+
+class TrackViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    private val trackNameView: TextView
+    private val artistNameView: TextView
+    private val trackTimeView: TextView
+    private val trackImage: ImageView
+
+    init {
+        trackNameView = itemView.findViewById(R.id.trackName)
+        artistNameView = itemView.findViewById(R.id.artistName)
+        trackTimeView = itemView.findViewById(R.id.trackTime)
+        trackImage = itemView.findViewById(R.id.trackImage)
+    }
+
+    fun bind(model: Track) {
+        trackNameView.text = model.trackName
+        artistNameView.text = model.artistName
+        trackTimeView.text = model.trackTime
+        Glide.with(itemView)
+            .load(model.artworkUrl100)
+            .placeholder(R.drawable.placeholder)
+            .centerCrop()
+            .transform(RoundedCorners(8))
+            .into(trackImage)
+    }
+}
