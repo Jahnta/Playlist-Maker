a535151a663acab0b2d68e9ca5a62aadfa999372
Jahnta <dabrusov@gmail.com>
2023-03-11 17:51:15 +0300

    Реализована смена темы в настройках с сохранением в Shared Preferences

---
+++ app/src/main/java/com/practicum/playlistmaker/App.kt
@@ -0,0 +1,30 @@
+package com.practicum.playlistmaker
+
+import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
+
+const val PLAYLIST_MAKER_PREFERENCES = "playlist_maker_preferences"
+const val THEMESWITCHER_KEY = "key_for_themeSwitcher"
+
+class App : Application() {
+
+    var darkTheme = false
+
+    override fun onCreate() {
+        super.onCreate()
+        val sharedPrefs = getSharedPreferences(PLAYLIST_MAKER_PREFERENCES, MODE_PRIVATE)
+        darkTheme = sharedPrefs.getBoolean(THEMESWITCHER_KEY, false)
+        (applicationContext as App).switchTheme(darkTheme)
+    }
+
+    fun switchTheme(darkThemeEnabled: Boolean) {
+        darkTheme = darkThemeEnabled
+        AppCompatDelegate.setDefaultNightMode(
+            if (darkThemeEnabled) {
+                AppCompatDelegate.MODE_NIGHT_YES
+            } else {
+                AppCompatDelegate.MODE_NIGHT_NO
+            }
+        )
+    }
+}
